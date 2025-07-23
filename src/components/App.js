d094daab7681baf258cae967b4d5b2473ca8aea3
Debra <debrawangeci@gmail.com>
2025-07-23 18:52:16 +0300

    add all changes

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,34 @@ import itemData from "../data/items";
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
+  function handleSearchChange(searchValue) {
+    setSearch(searchValue);
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList
+        items={items}
+        selectedCategory={selectedCategory}
+        onCategoryChange={handleCategoryChange}
+        search={search}
+        onSearchChange={handleSearchChange}
+      />
     </div>
   );
 }
 
 export default App;
+
